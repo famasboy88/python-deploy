a1fb678ad0f65c55381c5c3fd5e777696e19d72a
Kyle Yap <famasboy88@gmail.com>
2017-11-25 20:51:40 +0800

    testing in main

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-PORT = 8000
-DEBUG = True
-
-@app.errorhandler(400)
-def not_found(error):
-    return "Not Found."
-
-@app.route('/', methods=['GET'])
-def index():
-    return "Hello World."
-
-if __name__== '__main__':
-    app.run(port=PORT, debug=DEBUG)
+return "Hi"
